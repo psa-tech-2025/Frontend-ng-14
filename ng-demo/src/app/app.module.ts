0f10e27bffccd9720a87f6cbf38c6b4770acec7b
psa-tech-2025 <services.psatech@gmail.com>
2025-08-07 12:45:49 +0530

    "Updated"

--- ng-demo/src/app/app.module.ts
+++ ng-demo/src/app/app.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PopupComponent } from './common/component/popup/popup.component';
-import { DiolougeComponent } from './common/diolouge/diolouge/diolouge.component';
-import { ClickOutDirective } from './common/directive/click-out.directive';
-import { DropdownPipelineComponent } from './common/drop-pipe/dropdown-pipeline/dropdown-pipeline.component';
-import { CommaPipe } from './common/pipes/comma.pipe';
 import { MyCommonModule } from './common/common.module';
 
 @NgModule({
